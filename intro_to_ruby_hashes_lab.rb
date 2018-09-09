bf497de3109a53255f70c63f89f1251236be3f01
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 14:43:26 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,20 +31,20 @@ end
 
 monopoly_with_second_tier
 
-contacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
+# contacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
  
-puts contacts
-  #  >
-  {
-    "Jon Snow" => {
-     :name=>"Jon",
-     :email=>"jon_snow@thewall.we",
-     :favorite_ice_cream_flavors=>["chocolate", "vanilla", "mint chip"],
-     :address=>"The Lord Commander's Rooms, The Wall, Westeros"
-    },
-    "Freddy Mercury"=> {
-     :name=>"Freddy",
-     :email=>"freddy@mercury.com",
-     :favorite_ice_cream_flavors=> ["cookie dough", "mint chip"]
-    }
-  }
+# puts contacts
+#   #  >
+#   {
+#     "Jon Snow" => {
+#     :name=>"Jon",
+#     :email=>"jon_snow@thewall.we",
+#     :favorite_ice_cream_flavors=>["chocolate", "vanilla", "mint chip"],
+#     :address=>"The Lord Commander's Rooms, The Wall, Westeros"
+#     },
+#     "Freddy Mercury"=> {
+#     :name=>"Freddy",
+#     :email=>"freddy@mercury.com",
+#     :favorite_ice_cream_flavors=> ["cookie dough", "mint chip"]
+#     }
+#   }
