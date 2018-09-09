35809a127f69c35fb8a16e2a1267b8c3451163ae
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 15:20:05 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   monopoly = {:railroads => 
   {:pieces => 4, 
   :names=> {
-    :reading_railroad => 
+    :reading_railroad => {"mortgage_value" => "$100"},
     }, 
   :rent_in_dollars=> 
   {:one_piece_owned => 25, 
