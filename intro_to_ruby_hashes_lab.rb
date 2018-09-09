dd6bd10342dde1c45467431def4565691b233646
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 14:51:11 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,8 @@ end
 
 
 def monopoly_with_second_tier
-	monopoly[:railroads] = {} }
-	monopoly[:railroads][:pieces] = "kevin"
+	monopoly[:railroads] = {} 
+	monopoly[:railroads][:pieces] = """kevin"""
 
 end
 
